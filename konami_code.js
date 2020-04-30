b681a2d9d2e83bc216aec1857f427c0956b942fa
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-30 18:19:36 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,17 @@ const codes = [
   "a"
 ];
 
-function init() {
+function init()
+{
   // your code here
+  document.body.addEventListener("keydown", (event) => {
+    for(let i = 0; i < codes.length; i += 1)
+    {
+      if(event.key != codes[i])
+      {
+        console.log(`Nope!`)
+        return `Nope!`
+      }
+    }
+    alert(`W0W!!! U R L33T H4K3R!!! 1MPR3551V3!!!`)
 }

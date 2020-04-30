b4058105795b4afa481560a01b20834856bfc2fd
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-30 18:27:58 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,20 +17,21 @@ function init()
   // your code here
   document.body.addEventListener("keydown", (event) => {
 
-    // This is the function that would be invoked by the event listener.
-
-      const key = e.key;
-
-      if (key == codes[index]) {
-        index++;
-
+    const key = e.key;
+    if (key == codes[index]) 
+    {
+      index++;
       if (index == codes.length) 
       {
         alert(`W0W!!! U R L33T H4K3R!!! 1MPR3551V3!!!`)
         index = 0;
       }
-    } else {
+    }
+    else 
+    {
+      console.log(`nope`)
       index = 0;
     }
 
+  }
 }
